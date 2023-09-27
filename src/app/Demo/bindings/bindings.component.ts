09efe83743a99cb641f53abf542561ad608567c0
Evengyl <dark.evengyl@gmail.com>
2023-09-27 11:47:08 +0200

    add style bindings + home

--- src/app/Demo/bindings/bindings.component.ts
+++ src/app/Demo/bindings/bindings.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bindings',
   templateUrl: './bindings.component.html',
-
+  styleUrls: ['./bindings.component.scss']
 })
 export class BindingsComponent {
   //props one way binding
